30e9de75a844e229316beaf6bf8e8f223ba1ec86
wahidaleena <34679809+wahidaleena@users.noreply.github.com>
2019-06-24 13:05:11 +0530

    Update perform.cbl

--- perform.cbl
+++ perform.cbl
@@ -1,21 +1,21 @@
-IDENTIFICATION DIVISION.
-PROGRAM-ID. HELLO.
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. HELLO.
 
-PROCEDURE DIVISION.
-   A-PARA.
-   PERFORM DISPLAY 'IN A-PARA'
-   END-PERFORM.
-   PERFORM C-PARA THRU E-PARA.
+       PROCEDURE DIVISION.
+          A-PARA.
+          PERFORM DISPLAY 'IN A-PARA'
+          END-PERFORM.
+          PERFORM C-PARA THRU E-PARA.
    
-   B-PARA.
-   DISPLAY 'IN B-PARA'.
-   STOP RUN.
+          B-PARA.
+          DISPLAY 'IN B-PARA'.
+          STOP RUN.
    
-   C-PARA.
-   DISPLAY 'IN C-PARA'.
+          C-PARA.
+          DISPLAY 'IN C-PARA'.
    
-   D-PARA.
-   DISPLAY 'IN D-PARA'.
+          D-PARA.
+          DISPLAY 'IN D-PARA'.
    
-   E-PARA.
-   DISPLAY 'IN E-PARA'.
+          E-PARA.
+          DISPLAY 'IN E-PARA'.
